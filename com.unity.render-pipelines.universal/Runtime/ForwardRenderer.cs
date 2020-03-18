7dfbd2daafa90f180d2826dee95fff8b59ff01bf
tomzig16 <tzigmantavicius@gmail.com>
2020-03-18 11:13:08 +0200

    Check if color texture is needed after checking for depth

--- com.unity.render-pipelines.universal/Runtime/ForwardRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/ForwardRenderer.cs
@@ -175,6 +175,15 @@ namespace UnityEngine.Rendering.Universal
             bool createDepthTexture = cameraData.requiresDepthTexture && !requiresDepthPrepass;
             createDepthTexture |= (cameraData.renderType == CameraRenderType.Base && !cameraData.resolveFinalTarget);
 
+#if UNITY_ANDROID
+            if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.Vulkan)
+            {
+                // GLES can not use render texture's depth buffer with the color buffer of the backbuffer
+                // in such case we create a color texture for it too.
+                createColorTexture |= createDepthTexture;
+            }
+#endif
+
             // Configure all settings require to start a new camera stack (base camera only)
             if (cameraData.renderType == CameraRenderType.Base)
             {
@@ -458,18 +467,6 @@ namespace UnityEngine.Rendering.Universal
             bool requiresBlitForOffscreenCamera = cameraData.postProcessEnabled || cameraData.requiresOpaqueTexture || requiresExplicitMsaaResolve;
             if (isOffscreenRender)
                 return requiresBlitForOffscreenCamera;
-
-#if UNITY_ANDROID
-            if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.Vulkan)
-            {
-                if (!cameraData.requiresOpaqueTexture && cameraData.requiresDepthTexture)
-                {
-                    // GLES can not use render texture's depth buffer with the color buffer of the backbuffer
-                    // in such case we create a color texture for it too.
-                    return true;
-                }
-            }
-#endif
 
             return requiresBlitForOffscreenCamera || cameraData.isSceneViewCamera || isScaledRender || cameraData.isHdrEnabled ||
                    !isCompatibleBackbufferTextureDimension || !cameraData.isDefaultViewport || isCapturing ||
